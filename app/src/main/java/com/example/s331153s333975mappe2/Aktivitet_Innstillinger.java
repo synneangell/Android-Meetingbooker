672895e775d2d82181e46f5d2ea090bf9581c217
Nikola Sekiw <nnikolasekiw@users.noreply.github.com>
2020-10-07 23:06:51 +0200

    bare noe smått i preferanser

--- app/src/main/java/com/example/s331153s333975mappe2/Aktivitet_Innstillinger.java
+++ app/src/main/java/com/example/s331153s333975mappe2/Aktivitet_Innstillinger.java
@@ -1,15 +1,23 @@
 package com.example.s331153s333975mappe2;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 
 public class Aktivitet_Innstillinger extends PreferenceActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getFragmentManager().beginTransaction().replace(android.R.id.content, new PrefsFragment()).commit();
+
+    }
 
     public static class PrefsFragment extends PreferenceFragment {
         @Override
@@ -29,20 +37,19 @@ public class Aktivitet_Innstillinger extends PreferenceActivity {
     @Override
     public boolean onCreateOptionsMenu (Menu menu){
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_mote, menu);
+        inflater.inflate(R.menu.menu_innstillinger, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item){
         switch (item.getItemId()){
-            case R.id.kontakter:
-                //Her er det egentlig meningen at det skal sendes verdier til neste aktivitet ut ifra det man trykker på i listview
-                Intent i = new Intent(this, Aktivitet_Kontakt.class);
+            case R.id.mote:
+                Intent i = new Intent(this, Aktivitet_Mote.class);
                 startActivity(i);
                 break;
-            case R.id.innstillinger:
-                Intent i2 = new Intent(this, Aktivitet_Innstillinger.class);
+            case R.id.kontakter:
+                Intent i2 = new Intent(this, Aktivitet_Kontakt.class);
                 startActivity(i2);
                 break;
             default:
